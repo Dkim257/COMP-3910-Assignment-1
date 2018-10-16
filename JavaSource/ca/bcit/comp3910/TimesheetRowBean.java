029997983b8b76f1889d60a2608ffbc9a2015e6c
Daniel DiIorio <ddiiorio@my.bcit.ca>
2018-10-15 23:19:35 -0700

    Editing TimesheetRowBean to enable edit, add row

--- JavaSource/ca/bcit/comp3910/TimesheetRowBean.java
+++ JavaSource/ca/bcit/comp3910/TimesheetRowBean.java
@@ -1,18 +1,38 @@
 package ca.bcit.comp3910;
 
+import ca.bcit.infosys.timesheet.Timesheet;
 import ca.bcit.infosys.timesheet.TimesheetRow;
 import javax.inject.Named;
-
+import org.primefaces.event.CellEditEvent;
+import org.primefaces.event.RowEditEvent;
 import java.io.Serializable;
-
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 @Named("row")
 @ApplicationScoped
 public class TimesheetRowBean extends TimesheetRow implements Serializable {
+    
 
     public TimesheetRowBean() {
         super();
+    }
+    
+    @PostConstruct 
+    public void init() {
+
+    }
+    
+    public void onRowEdit(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Row Edited");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+    public void onRowCancel(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
     
 }
