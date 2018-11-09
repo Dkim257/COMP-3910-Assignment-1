77a9d9adf3b8e42c1010829608982ffd29666fbc
Daniel DiIorio <ddiiorio@my.bcit.ca>
2018-11-08 20:47:45 -0800

    made employee form access

---
+++ JavaSource/controller/EmployeeFormAccess.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import access.EmployeeManager;
+import models.Employees;
+
+@Named("users")
+@SessionScoped
+public class EmployeeFormAccess implements Serializable {
+
+    @Inject
+    private EmployeeManager mgr;
+    private List<Employees> list;
+
+    public List<Employees> getList() {
+        if (list == null)
+            refreshList();
+        return list;
+    }
+
+    public void setList(List<Employees> employees) {
+        this.list = employees;
+    }
+
+    private void refreshList() {
+        list = mgr.getAll();
+    }
+
+}
